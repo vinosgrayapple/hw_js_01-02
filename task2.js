ae3db6c2dd6a5da50a0ee2c7f49c8903bdc00952
Serhii Komarychev <komarichev@gmail.com>
2016-08-01 02:12:14 +0300

    change task2.js

--- task2.js
+++ task2.js
@@ -1,10 +1,15 @@
+var arrName = [];
+var i = 0;
+var checkName = false;
 
+/* Заполняем массив имен! */ 
+alert('Вводим 5 имен!! \n');
+while ( i < 5 ){
+	arrName.push(prompt("Введите имя #"+(++i)));
+}
 /* Вводим с помощью prompt имя пользователя */ 
-
 var yourName = prompt('Введите Ваше имя:');
-
 /* Введенное имя, циклом сравниваем с именами в массиве */ 
-
 for (var i = 0; i < arrName.length; i++) {
 	if (arrName[i] == yourName) checkName = true;
 }
